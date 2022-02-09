8460b1547502a45d45577b3ec10ed8f1b3975c78
= <=>
2022-02-09 09:50:20 +0100

    added lazy loading to the script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script';
 import Head from 'next/head'
 // 1. import `ChakraProvider` component
 import { ChakraProvider, Theme } from "@chakra-ui/react"
@@ -7,6 +8,12 @@ import { ChakraProvider, Theme } from "@chakra-ui/react"
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+         <Script 
+        id="mcjs"
+        strategy="lazyOnload"
+      >
+        {`!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/c72b39410028c4afc5fb0d25e/5021bb74d3f1b911e04acf50d.js");`}
+      </Script>
        <Component {...pageProps} />
     </ChakraProvider>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -74,12 +74,6 @@ const Home: NextPage = () => {
 
   return (
     <div className="w-screen h-auto">
-      <Script 
-        id="mcjs"
-        strategy="afterInteractive"
-      >
-        {`!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/c72b39410028c4afc5fb0d25e/5021bb74d3f1b911e04acf50d.js");`}
-      </Script>
       <MyModal open={showModal} onClose={setShowModal} />
       <Head>
         <title>Pass APP</title>
